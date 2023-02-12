9158a337be6aba1f0b2c5e3b611395887892d114
dongeun <kdedong12@gmail.com>
2023-02-12 23:34:22 +0900

    docs upload

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: "/vue-example",
+  outputDir: "./docs",
   transpileDependencies: [
     'vuetify'
   ],
